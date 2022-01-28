5faf387149aab63d888e94107266c16b97fb4f86
Sarthak044 <46262012+Sarthak044@users.noreply.github.com>
2022-01-28 23:08:56 +0530

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -1,50 +1,130 @@
 package main
 
+
+
 import (
-	"bufio"
+
 	"fmt"
-	"os"
+
 	"os/exec"
+
 )
+
+
 
 var input string
 
-func main() {
-	fmt.Println("Welcome to the WIRELESS ADAPTER MODE CHANGER")
-	fmt.Println("Make sure your WIRELESS ADAPTER SUPPORTS MODES")
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("[+] Enter the interface")
-	input, _ := reader.ReadString('\n')
 
-	if input == "eth0" && input == "lo" {
+
+func main() {
+
+
+
+	fmt.Println("Welcome to the WIRELESS ADAPTER MODE CHANGER")
+
+	fmt.Println("Make sure your WIRELESS ADAPTER SUPPORTS MODES")
+
+	fmt.Println("[+] Enter the interface")
+
+	
+
+	fmt.Scanln(&input)
+
+	fmt.Println("ifconfig " + input + " down")
+
+
+
+	if input == "eth0" || input == "lo" {
 
 		fmt.Println("[+]Not a Wireless adapter")
+
 		fmt.Println("[+]BYE BYE !")
+
 	} else {
+
 		fmt.Println("Select the Mode\n1.Monitor Mode\n2.Managed Mode")
-		Mode_number, _ := reader.ReadString('\n')
-		if Mode_number == "1" {
+
+		var Mode_number int8
+
+		fmt.Scanln(&Mode_number)
+
+		if Mode_number == 1 {
+
 			process1()
-		} else if Mode_number == "2" {
+
+		} else if Mode_number == 2 {
+
 			process2()
+
 		} else {
+
 			fmt.Println("[-] Enter a valid Option", "[+]Exiting")
+
 		}
+
 	}
+
 }
+
+
 
 func process1() {
+
 	fmt.Println("[+]Changing the mode to Monitor")
-	exec.Command("ifconfig" + input + "down")
-	exec.Command("iwconfig" + input + "mode" + "monitor")
-	exec.Command("ifconfig" + input + "up")
+
+	c1,_ := exec.Command("sudo","ifconfig",input,"down").Output()
+
+	c2,_ := exec.Command("sudo","iwconfig",input,"mode","monitor").Output()
+
+	c3,_ := exec.Command("sudo","ifconfig",input,"up").Output()
+
+
+
+	out1 := string(c1[:])
+
+    fmt.Println(">",out1)
+
+	out2 := string(c2[:])
+
+    fmt.Println(">>",out2)
+
+	out3 := string(c3[:])
+
+    fmt.Println(">>>",out3)
+
+	
+
 	fmt.Println("[+] Mode changed Successfully")
+
 }
 
+
+
 func process2() {
+
 	fmt.Println("[+]Changing the Mode to Managed")
-	exec.Command("ifconfig" + input + "down")
-	exec.Command("iwconfig" + input + "mode" + "managed")
-	exec.Command("ifconfig" + input + "up")
+
+	c1,_ := exec.Command("sudo","ifconfig",input,"down").Output()
+
+	c2,_ := exec.Command("sudo","iwconfig",input,"mode","managed").Output()
+
+	c3,_ := exec.Command("sudo","ifconfig",input,"up").Output()
+
+
+
+	out1 := string(c1[:])
+
+    fmt.Println(">",out1)
+
+	out2 := string(c2[:])
+
+    fmt.Println(">>",out2)
+
+	out3 := string(c3[:])
+
+    fmt.Println(">>>",out3)
+
 	fmt.Println("[+] Mode changed Successfully")
+
 }
+
